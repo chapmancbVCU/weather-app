73d913d61aa756b1d8ec9b427514254f0ce51e67
Chad Chapman <chad.chapman2010@gmail.com>
2023-02-26 13:00:20 -0500

    commit

--- src/API.js
+++ src/API.js
@@ -3,10 +3,17 @@
  * @author Chad Chapman
  */
 export class API {
+    /**
+     * Default constructor.
+     */
     constructor() {
         this.weatherKey = '37848b175665f0d44acbf1f3d3b01259';
     }
 
+    /**
+     * Getter function for open weather API key.
+     * @returns The API key for open weather.
+     */
     getWeatherKey() {
         return this.weatherKey;
     }
